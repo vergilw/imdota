2b3b4d5b609d2252c5146cc9adfae9aa2da53703
vergilw <antjun@126.com>
2018-09-20 18:49:19 +0800

    feat: add editor staff

--- imdota_django/reasoning/urls.py
+++ imdota_django/reasoning/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
+    path('createeditor', views.createeditor, name='createeditor'),
 ]
--- imdota_django/reasoning/views.py
+++ imdota_django/reasoning/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import login
+from django.contrib.auth.models import Group
 from . import models
 
 # Create your views here.
@@ -42,7 +43,8 @@ def signup(request):
                 return JsonResponse({"errorCode": "userAlreadyExist"}, status=400)
 
             except:
-                user = models.User(username=username, password=password)
+                user = models.User(username=username)
+                user.set_password(password)
                 user.save()
                 login(request, user)
                 return JsonResponse({"message": "success", "gender": user.gender}, status=200)
@@ -54,3 +56,30 @@ def signup(request):
         return JsonResponse({"errorCode": "incorrectRequestMethod"}, status=400)
 
 
+def createeditor(request):
+    if request.method == "GET":
+        if request.user.is_superuser:
+            username = request.GET.get("username", None)
+            password = request.GET.get("password", None)
+            if username and password:
+                try:
+                    models.User.objects.get(username=username)
+                    return JsonResponse({"errorCode": "userAlreadyExist"}, status=400)
+
+                except:
+                    user = models.User(username=username)
+                    user.set_password(password)
+                    user.is_staff = True
+                    user.save()
+                    editorGroup = Group.objects.get(name='editor')
+                    editorGroup.user_set.add(user)
+                    return JsonResponse({"message": "created success"}, status=201)
+
+            else:
+                return JsonResponse({"errorCode": "missingParameters"}, status=422)
+
+        else:
+            return JsonResponse({"errorCode": "unauthorized"}, status=401)
+
+    else:
+        return JsonResponse({"errorCode": "incorrectRequestMethod"}, status=400)
